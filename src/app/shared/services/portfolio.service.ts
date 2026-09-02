75c56413b1d1d28fb1bd372e1d9f2b3360c89034
NokrisMx <guevaraaldo44@gmail.com>
2026-09-02 11:42:38 -0600

    feat: refactor PortfolioService para usar un observable compartido y optimizar la gestión de la respuesta de la API

--- src/app/shared/services/portfolio.service.ts
+++ src/app/shared/services/portfolio.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, ReplaySubject, share } from 'rxjs';
 import { Portfolio } from '../models/portfolio.models';
 
 const API_URL = 'https://69d19cab5043d95be971190e.mockapi.io/api/v1/portfolio';
@@ -11,14 +11,24 @@ const API_URL = 'https://69d19cab5043d95be971190e.mockapi.io/api/v1/portfolio';
 export class PortfolioService {
   private readonly http = inject(HttpClient);
 
-  getPortfolio(): Observable<Portfolio> {
-    return this.http.get<Portfolio[]>(API_URL).pipe(
+  private readonly portfolio$: Observable<Portfolio> = this.http
+    .get<Portfolio[]>(API_URL)
+    .pipe(
       map((items) => {
         if (!items || items.length === 0) {
           throw new Error('La respuesta de la API está vacía');
         }
         return items[0];
       }),
+      share({
+        connector: () => new ReplaySubject(1),
+        resetOnError: true,
+        resetOnComplete: false,
+        resetOnRefCountZero: false,
+      }),
     );
+
+  getPortfolio(): Observable<Portfolio> {
+    return this.portfolio$;
   }
 }
